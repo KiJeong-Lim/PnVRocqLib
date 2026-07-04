685416d6860772e3a80c9519f0e3c209598ff95f
KiJeong-Lim <gijungdduk@naver.com>
2026-07-04 19:32:03 +0900

    Refactor

--- theories/Prelude/X.v
+++ theories/Prelude/X.v
@@ -2,9 +2,6 @@ Require Import PnV.Prelude.Prelude.
 
 Notation "lhs ≠ rhs" := (~ (lhs = rhs)) : type_scope.
 
-Ltac use_exact H :=
-  revert H; clear; intro H; exact H.
-
 Tactic Notation "use" uconstr( H ) :=
   hexploit H; cycle -1; [i | eauto with * ..]; cycle 1.
 
@@ -12,31 +9,31 @@ Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) :=
   hexploit H; cycle -1; [intros p | try eassumption ..]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) ident( H4 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3 | use_exact H4]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3 | exact H4]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) ident( H4 ) ident( H5 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3 | use_exact H4 | use_exact H5]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3 | exact H4 | exact H5]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) ident( H4 ) ident( H5 ) ident( H6 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3 | use_exact H4 | use_exact H5 | use_exact H6]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3 | exact H4 | exact H5 | exact H6]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) ident( H4 ) ident( H5 ) ident( H6 ) ident( H7 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3 | use_exact H4 | use_exact H5 | use_exact H6 | use_exact H7]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3 | exact H4 | exact H5 | exact H6 | exact H7]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) ident( H4 ) ident( H5 ) ident( H6 ) ident( H7 ) ident( H8 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3 | use_exact H4 | use_exact H5 | use_exact H6 | use_exact H7 | use_exact H8]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3 | exact H4 | exact H5 | exact H6 | exact H7 | exact H8]; cycle 1.
 
 Tactic Notation "use" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) ident( H2 ) ident( H3 ) ident( H4 ) ident( H5 ) ident( H6 ) ident( H7 ) ident( H8 ) ident( H9 ) :=
-  hexploit H; cycle -1; [intros p | use_exact H1 | use_exact H2 | use_exact H3 | use_exact H4 | use_exact H5 | use_exact H6 | use_exact H7 | use_exact H8 | use_exact H9]; cycle 1.
+  hexploit H; cycle -1; [intros p | exact H1 | exact H2 | exact H3 | exact H4 | exact H5 | exact H6 | exact H7 | exact H8 | exact H9]; cycle 1.
 
 Tactic Notation "use?" uconstr( H ) "as" simple_intropattern( p ) "with" ident( H1 ) :=
   hexploit H; cycle -1; [intros p | try (revert H1; clear; i; eassumption) ..]; cycle 1.
